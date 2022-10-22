28cd6790ae6686e307fd6b6aeaf34492bb46da4a
ginstone <hjg719@139.com>
2022-10-22 12:29:34 +0800

    publish to npm

--- enhance/StorageEnhance.ts
+++ enhance/StorageEnhance.ts
@@ -3,11 +3,11 @@
  */
 
 
-Storage.prototype.put = function (key, value) {
+Storage.prototype.put = function (key:string, value:any) {
     this.setItem(key, JSON.stringify(value))
 }
 
-Storage.prototype.get = function (key) {
+Storage.prototype.get = function (key:string) :object|undefined{
     let item = this.getItem(key);
     if (item) {
         return JSON.parse(item)
@@ -18,13 +18,13 @@ Storage.prototype.get = function (key) {
  * 获取指定前缀的key
  * @param prefix 前缀
  */
-Storage.prototype.getKeyByPrefix = function (prefix) {
+Storage.prototype.getKeyByPrefix = function (prefix:string):Array<string> {
     return Object.keys(this.storage).filter(key => key.startsWith(prefix))
 }
 /**
  * 获取指定前缀的value
  * @param prefix 前缀
  */
-Storage.prototype.getValueByPrefix = function (prefix) {
+Storage.prototype.getValueByPrefix = function (prefix:string) :Array<object> {
     return Object.keys(this.storage).filter(key => key.startsWith(prefix)).map(key => this.get(key))
 }

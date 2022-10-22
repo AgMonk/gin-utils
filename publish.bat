8645784b05bdad80317c83d9c21281be52d5db22
ginstone <hjg719@139.com>
2022-10-22 10:26:08 +0800

    publish to npm

--- publish.bat
+++ publish.bat
@@ -1,7 +1,7 @@
 git add .
 git commit -m "publish to npm"
 npm version patch
-echo f |xcopy package.json dist\package.json
+echo f|xcopy package.json dist\package.json
 cd dist
 npm publish --access public
 pause

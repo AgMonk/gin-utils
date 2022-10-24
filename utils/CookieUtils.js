4bf85bfa079a87c34d416edec7c73ee58b3b6539
ginstone <hjg719@139.com>
2022-10-24 09:36:54 +0800

    改为对象

--- utils/CookieUtils.js
+++ utils/CookieUtils.js
@@ -1,27 +1,29 @@
-export const getCookieMap = (cookieString = document.cookie) => {
-    let map = {};
-    cookieString.split("; ").forEach(line => {
-        let s = line.split("=");
-        map[s[0]] = s[1];
-    })
-    return map;
-}
+export class CookieUtils{
+    static getMap = (cookieString = document.cookie) => {
+        let map = {};
+        cookieString.split("; ").forEach(line => {
+            let s = line.split("=");
+            map[s[0]] = s[1];
+        })
+        return map;
+    }
 
-export const setCookie = (key, value, expires, path) => {
-    let day = 24 * 60 * 60 * 1000;
-    let date = new Date();
-    date.setTime(date.getTime() + expires * day);
-    document.cookie = `${key}=${value};path=${path};expires=${date.toUTCString()}`;
-}
+    static set = (key, value, expires, path) => {
+        let day = 24 * 60 * 60 * 1000;
+        let date = new Date();
+        date.setTime(date.getTime() + expires * day);
+        document.cookie = `${key}=${value};path=${path};expires=${date.toUTCString()}`;
+    }
 
-export const getCookie = (key) => {
-    return getCookieMap()[key];
-}
+    static get = (key) => {
+        return CookieUtils.getMap()[key];
+    }
 
-export const setCookies = (cookieString, expires, path) => {
-    let cookies = cookieString.split("; ");
-    cookies.forEach(cookie => {
-        let s = cookie.split("=");
-        setCookie(s[0], s[1], expires, path)
-    })
+    static setBatch = (cookieString, expires, path) => {
+        let cookies = cookieString.split("; ");
+        cookies.forEach(cookie => {
+            let s = cookie.split("=");
+            CookieUtils.set(s[0], s[1], expires, path)
+        })
+    }
 }
--- utils/ObjectUtils.js
+++ utils/ObjectUtils.js
@@ -1,17 +1,17 @@
-//复制一个对象
-export const copyObj = (obj) => JSON.parse(JSON.stringify(obj))
-
-// 获取对象类型
-export const getTypeOf = function (obj) {
-    let type = Object.prototype.toString.call(obj);
-    return type.replace("[object ", "").replace("]", "");
-}
-
-// 将对象转换为数组
-export const obj2Array = (obj) => {
-    let array = [];
-    Object.keys(obj).forEach(key => {
-        array.push(obj[key])
-    })
-    return array;
+export class ObjectUtils {
+    //复制一个对象
+    static clone = (obj) => JSON.parse(JSON.stringify(obj))
+    // 获取对象类型
+    static getTypeOf = function (obj) {
+        let type = Object.prototype.toString.call(obj);
+        return type.replace("[object ", "").replace("]", "");
+    }
+    // 将对象转换为数组
+    static toArray = (obj) => {
+        let array = [];
+        Object.keys(obj).forEach(key => {
+            array.push(obj[key])
+        })
+        return array;
+    }
 }

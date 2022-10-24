3b153b9dcaabb8c4907ed972835155b59adc146a
ginstone <hjg719@139.com>
2022-10-24 12:02:56 +0800

    1.0.36 数组工具类：去重

---
+++ utils/ArrayUtils.ts
@@ -0,0 +1,17 @@
+export class ArrayUtils {
+    /**
+     * 去重
+     * @param source 来源数组
+     * @param getHash 根据数组成员生成唯一编号的方法
+     */
+    distinct(source: Array<any>,getHash:(item: any)=>string): Array<any> {
+        let map = new Map<string,any>();
+        source.forEach(item=>{
+            let hash = getHash(item);
+            if (!map.has(hash)){
+                map.set(hash,item);
+            }
+        })
+        return [...map.values()];
+    }
+}
